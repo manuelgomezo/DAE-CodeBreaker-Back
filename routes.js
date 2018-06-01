c67a33b8162f3d14f144e5401a7ef0bb5aed7be6
manuelgomezo <manuel.gomezo@udea.edu.co>
2018-06-01 18:36:04 -0500

    Hotfix: route fix

--- routes.js
+++ routes.js
@@ -7,13 +7,13 @@ const instanceCodeBreaker = new codebreaker("1234");
 
 api.get('/codebreaker/:num',function (req, res) {
     let num = req.params.num;
-    let resultado = instanceCodeBreaker.compare(num);
+    let result = instanceCodeBreaker.compare(num);
     res.status(200).send({ result });
 });
 
 api.get('/codebreaker/secret/:num',function (req, res) {
     let num = req.params.num;
-    let resultado = instanceCodeBreaker.setSecret(num);
+    let result = instanceCodeBreaker.setSecret(num);
     res.status(200).send({ result });
 });
 
